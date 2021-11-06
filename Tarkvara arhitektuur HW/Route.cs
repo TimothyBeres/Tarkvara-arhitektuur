7e3e244013fb2069c625852bb52f7b4e03de1a84
Timothy Beres <timothy.beres@helmes.ee>
2021-11-06 17:36:19 +0200

    init

---
+++ Tarkvara arhitektuur HW/Route.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarkvara_arhitektuur_HW
+{
+    public class Route
+    {
+        private List<Point> points;
+        public Route ()
+        {
+            CreateRoute();
+        }
+        public List<Point> GetPoints()
+        {
+            return points;
+        }
+        private void CreateRoute()
+        {
+            points = new List<Point>();
+        }
+        public void AddPoint(double x, double y, int index)
+        {
+            if (index < 0 || index > points.Count)
+            {
+                return;
+            }
+            points.Insert(index, new Point(x, y));
+        }
+        
+        public void RemovePoint(int index)
+        {
+            if (index < 0 || index >= points.Count)
+            {
+                return;
+            }
+            points.RemoveAt(index);
+        }
+        public double GetLength()
+        {
+            if(points.Count <= 1)
+            {
+                return 0;
+            }
+            double routeDistance = 0;
+            for(int i = 0; i < points.Count - 1; i++)
+            {
+                routeDistance += points[i].Distance(points[i + 1]);
+            }
+            return routeDistance;
+        }
+    }
+}
---
+++ Tests/RouteTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using Tarkvara_arhitektuur_HW;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace RouteTests
+{
+    public class RouteTests
+    {
+        private Route route;
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public void TestCreateRoute()
+        {
+            route = new Route();
+            Assert.IsNotNull(route.GetPoints());
+        }
+        [Test]
+        public void TestAddPoint()
+        {
+            route = new Route();
+            route.AddPoint(10, 20, 2);
+            Assert.IsEmpty(route.GetPoints());
+            
+            route.AddPoint(10, 40, -2);
+            Assert.IsEmpty(route.GetPoints());
+
+            route.AddPoint(10, 20, 0);
+            route.AddPoint(10, 15, 0);
+            Assert.AreEqual(2, route.GetPoints().Count);
+        }
+        [Test]
+        public void TestRemovePoint()
+        {
+            route = new Route();
+            route.AddPoint(10, 20, 0);
+            route.RemovePoint(1);
+            Assert.AreEqual(1, route.GetPoints().Count);
+
+            route.RemovePoint(-1);
+            Assert.AreEqual(1, route.GetPoints().Count);
+
+            route.RemovePoint(0);
+            Assert.IsEmpty(route.GetPoints());
+
+            Point staysInRoute = new Point(8, 2);
+            route.AddPoint(5, 7, 0);
+            route.AddPoint(8, 2, 1);
+            route.RemovePoint(0);
+            Assert.AreEqual(1, route.GetPoints().Count);
+            Assert.AreEqual(JsonConvert.SerializeObject(route.GetPoints()[0]), JsonConvert.SerializeObject(staysInRoute));
+
+        }
+        [Test]
+        public void TestGetLength()
+        {
+            route = new Route();
+            for (int i = 0; i < 4; i++)
+            {
+                
+            }
+        }
+
+    }
+}
